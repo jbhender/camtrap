a3fe99fbc5b0bb0dc30c57662f931b4a6e6be601
James Henderson <jbhender@umich.edu>
2017-02-25 08:42:38 -0500

    Fixed roxygen comment style.

--- R/print.overlapPermObj.R
+++ R/print.overlapPermObj.R
@@ -1,10 +1,10 @@
-##' S3 method print method for overlapPermObj
-##' 
-##' Tidily print the list created by overlapPerm.
-##' 
-##' @param obj An object output by \code{\link{overlapPerm}}.
-##' @param obj The number of digits to display in the printed table.
-##' @export
+#' S3 method print method for overlapPermObj
+#' 
+#' Tidily print the list created by overlapPerm.
+#' 
+#' @param obj An object output by \code{\link{overlapPerm}}.
+#' @param obj The number of digits to display in the printed table.
+#' @export
 print.overlapPermObj <- function(obj,digits=3){
   if(!{'overlapPermObj' %in% class(obj)}){
     warning('Object is not of class "overlapPermObj".\n')
