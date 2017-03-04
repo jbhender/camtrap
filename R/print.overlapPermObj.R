add43775f11450bf1cb33e83186eeedb9138709a
James Henderson <jbhender@umich.edu>
2017-03-04 07:57:55 -0500

    Updated message.

--- R/print.overlapPermObj.R
+++ R/print.overlapPermObj.R
@@ -1,6 +1,6 @@
 #' S3 method print method for overlapPermObj
 #' 
-#' Tidily print the list created by overlapPerm.
+#' Tidily print the list created by \code{\link{overlapPerm}}.
 #' 
 #' @param obj An object output by \code{\link{overlapPerm}}.
 #' @param obj The number of digits to display in the printed table.
@@ -10,10 +10,9 @@ print.overlapPermObj <- function(obj,digits=3){
     warning('Object is not of class "overlapPermObj".\n')
   }
   print(round(obj$table,digits))
-  sides <- with(obj,ifelse(two.sided,'Two-sided','One-sided'))
-  type  <- with(obj,ifelse(parametric,'parametric','non-parametric'))
-  
-  s <- sprintf('%s permutation tests (%s) for difference from refrence.\n',
-               sides,type)
-  cat(s)
+  sides <- with(obj,ifelse(two.sided,'two-sided','one-sided'))
+  type  <- with(obj,ifelse(parametric,'Parametric','Non-parametric'))
+  s <- sprintf('%s permutation tests for difference from refrence.\n',type)
+  s1 <- sprintf('For the excess the test is %s.\n',sides)
+  cat(s,s1,sep='')
 }
